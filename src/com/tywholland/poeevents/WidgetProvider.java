2751663e945ec8e222b8cc32cc1f4b88de3a08ca
tyler <tyler@doubledutch.me>
2013-07-16 10:37:40 -0700

    Maybe fix for widget not updating

--- src/com/tywholland/poeevents/WidgetProvider.java
+++ src/com/tywholland/poeevents/WidgetProvider.java
@@ -3,7 +3,6 @@ package com.tywholland.poeevents;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.util.Log;
 
 public class WidgetProvider extends AppWidgetProvider {
 
@@ -11,9 +10,7 @@ public class WidgetProvider extends AppWidgetProvider {
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 			int[] appWidgetIds) {
 
-		Log.d("POE", "in provider onUpdate");
-		PoEUtil.updateWidget(context);
-		Log.d("POE", "after update widget in provider");
+	    new EventRequestTask(context).execute();
 
 		super.onUpdate(context, appWidgetManager, appWidgetIds);
 	}
